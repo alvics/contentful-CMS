22b16b115206717922d715d5eeab97a5e16b861a
Allen Pavic <alvics71@hotmail.com>
2018-08-28 11:58:30 +1000

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   contentful: {
-    spaceId: '3bh985ibn6ee',
-    token: 'a8801431cea8f387eb2c86780b2c945508ab0fc4baf2b272e7fdb4c394375efa'
+    spaceId: 'id_numbers,
+    token: 'your token'
   }
 };
