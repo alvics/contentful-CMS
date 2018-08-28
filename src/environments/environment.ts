444af721cb7166305ae6dedc5052747fde319406
alvics <alvics71@hotmail.com>
2018-08-28 12:17:37 +1000

    update readme

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,8 +6,8 @@ export const environment = {
   production: false,
 
   contentful: {
-    spaceId: '',
-    token: 'your token'
+    spaceId: '3bh985ibn6ee',
+    token: 'a8801431cea8f387eb2c86780b2c945508ab0fc4baf2b272e7fdb4c394375efa'
   }
 };
 
